db4ecdba5946eadfa945179296077497fcd103f9
Vachirakorn Rungsikawanich <vachirakorn@hotmail.com>
2016-04-11 21:50:59 +0700

    เพิ่มระบบ login ก่อนเข้าห้อง chat

--- app.js
+++ app.js
@@ -15,13 +15,6 @@ var con = mysql.createConnection({
     database: "chatdb"
 });
 
-
-
-// routing
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
-});
-
 // usernames which are currently connected to the chat
 var usernames = {};
 
@@ -38,6 +31,7 @@ io.sockets.on('connection', function(socket) {
         socket.room = 'room1';
         // add the client's username to the global list
         usernames[username] = username;
+        console.log(usernames);
         // send client to room 1
         socket.join('room1');
         // echo to client they've connected
@@ -110,3 +104,100 @@ io.sockets.on('connection', function(socket) {
         socket.leave(socket.room);
     });
 });
+
+//facebook login Passport
+var passport = require('passport');
+var Strategy = require('passport-facebook').Strategy;
+var check = require('connect-ensure-login');
+
+// Configure the Facebook strategy for use by Passport.
+//
+// OAuth 2.0-based strategies require a `verify` function which receives the
+// credential (`accessToken`) for accessing the Facebook API on the user's
+// behalf, along with the user's profile.  The function must invoke `cb`
+// with a user object, which will be set at `req.user` in route handlers after
+// authentication.
+passport.use(new Strategy({
+    clientID: 1549122352049988,
+    clientSecret: '221c989545ba03147c82812eceaa8b30',
+    callbackURL: 'http://localhost:8080/login/facebook/return',
+    profileFields: ['id', 'name', 'email','birthday', 'displayName', 'about', 'gender' , 'profileUrl','photos']
+
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    // In this example, the user's Facebook profile is supplied as the user
+    // record.  In a production-quality application, the Facebook profile should
+    // be associated with a user record in the application's database, which
+    // allows for account linking and authentication with other identity
+    // providers.
+
+    return cb(null, profile);
+  }));
+
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.  However, due to the fact that this
+// example does not have a database, the complete Twitter profile is serialized
+// and deserialized.
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+// Configure view engine to render EJS templates.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+// Use application-level middleware for common functionality, including
+// logging, parsing, and session handling.
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+
+// Initialize Passport and restore authentication state, if any, from the
+// session.
+app.use(passport.initialize());
+app.use(passport.session());
+
+//socket routing
+app.get('/chat',check.ensureLoggedIn('/login'), function(req, res) {
+    res.render('chat',{ user: req.user});
+});
+//login routes.
+app.get('/',
+  function(req, res) {
+    res.render('home', { user: req.user });
+  });
+
+app.get('/login',
+  function(req, res){
+    res.render('login');
+  });
+
+app.get('/login/facebook',
+  passport.authenticate('facebook'));
+
+app.get('/login/facebook/return',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    res.redirect('/');
+  });
+
+
+app.get('/profile',
+  check.ensureLoggedIn('/login'),
+  function(req, res){
+    console.dir(req.user);
+    console.log(req.user.photos[0].value);
+
+    res.render('profile', { user: req.user });
+  });
